b997ded66151941acdeb6e16521d9807a418afd8
DuncanCaplan <duncan.caplan@outlook.com>
2023-09-13 16:37:10 +0100

    cors issue

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 //change the site name
 const allowedOrigins = [
     "https://think-fast.onrender.com",
+    "https://main--peppy-babka-559469.netlify.app",
     "http://127.0.0.1:5500",
     "http://localhost:3500",
     "http://localhost:3000",
