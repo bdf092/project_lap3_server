e219c1f59d415e72ab5d2ea3abff709970e491e1
eduzgun <emreduz00@gmail.com>
2023-09-13 11:00:30 +0100

    refactored controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,57 +1,32 @@
-const bcrypt = require('bcrypt');
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require('../models/User');
 
-async function register (req, res) {
-    try {
-        const data = req.body;
-
-         // Generate a salt with a specific cost
-        const salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS));
-
-         // Hash the password
-        data["password"] = await bcrypt.hash(data["password"], salt);
-        
-        const result = await User.create(data);
-
-        res.status(201).send(result);
-    } catch (err) {
-        res.status(400).json({"error": err.message})
-    }
-};
-
-
-async function login(req, res) {
-    const data = req.body;
-    try {
-        const user = await User.getByEmail(data.email);
-
-        if (!user) {
-            throw new Error("User not found.");
-        }
-
-        const authenticated = await bcrypt.compare(data.password, user.password);
-
-        if (!authenticated) {
-            throw new Error("Incorrect password.");
-        } else {
-            // jwt signing will go here
-            const newToken = jwt.sign(
-                {
-                    "id": user.id,
-                    "username": user.username
-                },
-                process.env.TOKEN_KEY,
-                { expiresIn: "2h"}
-            );
-            res.status(200).json({
-                "message": "Login successful",
-                "token": newToken,
-            });
-        }
-    } catch (err) {
-        res.status(403).json({"error": err.message});
-    }
+const getAllUsers = async (req, res) => {
+    const users = await User.find();
+    if (!users) return res.status(204).json({ 'message': 'No users found' });
+    res.json(users);
 }
 
-module.exports = {register, login}
+const deleteUser = async (req, res) => {
+    if (!req?.body?.id) return res.status(400).json({ "message": 'User ID required' });
+    const user = await User.findOne({ _id: req.body.id }).exec();
+    if (!user) {
+        return res.status(204).json({ 'message': `User ID ${req.body.id} not found` });
+    }
+    const result = await user.deleteOne({ _id: req.body.id });
+    res.json(result);
+}
+
+const getUser = async (req, res) => {
+    if (!req?.params?.id) return res.status(400).json({ "message": 'User ID required' });
+    const user = await User.findOne({ _id: req.params.id }).exec();
+    if (!user) {
+        return res.status(204).json({ 'message': `User ID ${req.params.id} not found` });
+    }
+    res.json(user);
+}
+
+module.exports = {
+    getAllUsers,
+    deleteUser,
+    getUser
+}
--- models/User.js
+++ models/User.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    roles: {
+        User: {
+            type: Number,
+            default: 2001
+        },
+        Editor: Number,
+        Admin: Number
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    refreshToken: String
 });
 
+/*
 userSchema.statics.getById = async function(id) {
     const user = await this.findById(id);
     if (!user) {
@@ -36,6 +53,7 @@ userSchema.statics.createUser = async function(data) {
     await newUser.save();
     return newUser;
 };
+*/
 
 const User = mongoose.model('User', userSchema);
 
