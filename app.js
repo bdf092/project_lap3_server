a4b6ca53a23b436dda4a075b6d636e6d488f91f6
eduzgun <emreduz00@gmail.com>
2023-09-13 11:14:03 +0100

    more routes added

--- app.js
+++ app.js
@@ -17,13 +17,36 @@ mongoose.connect(process.env.DB_URI, {
     console.error('Error connecting to MongoDB:', err);
 });
 
+// custom middleware logger
+app.use(logger);
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+//middleware for cookies
+app.use(cookieParser());
 
 
-app.use(cors())
-app.use(express.json())
-app.use(logger('dev'))
-app.use("/users", userRouter)
-app.use("/quizzes", quizzesRouter)
+app.use('/', require('./routes/root'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
+
+app.use(verifyJWT);
+app.use('/employees', require('./routes/api/employees'));
+app.use('/users', require('./routes/api/users'));
+
 app.get('/', (req,res) => {
     res.json({
         name: "Revision API",
--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,4 +1,4 @@
-const User = require('../model/User');
+const User = require('../models/User');
 
 const handleLogout = async (req, res) => {
     // On client, also delete the accessToken
